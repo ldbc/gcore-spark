e71f89c922deff36949a59203f74c7fb4eea1be4
Georgiana Diana <georgiana.ciocirdel@cwi.nl>
2018-01-31 18:39:07 +0100

    Replace None with scala.None, to avoid confusion between ir.None and scala.None.

--- src/main/scala/ir/IrException.scala
+++ src/main/scala/ir/IrException.scala
@@ -1,5 +1,5 @@
 package ir
 
 final case class IrException(private val message: String = "",
-                             private val cause: Throwable = None.orNull)
+                             private val cause: Throwable = scala.None.orNull)
   extends Exception
