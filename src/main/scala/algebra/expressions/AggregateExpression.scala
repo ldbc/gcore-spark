45e76bcda02ecc520bf1bb3357858d6ca38d625b
CWI Centrum Wiskunde Cwicentrumwiskundeinformatica Informatica <cwicentrumwiskundeinformatica@CWIs-MacBook-Pro.local>
2018-08-31 13:49:44 -0300

    - add license blurb - if others add code to a file, please add notice of this in the copyright list   at the top of the modified file e.g. - Universidad de Talca (2018)

--- src/main/scala/algebra/expressions/AggregateExpression.scala
+++ src/main/scala/algebra/expressions/AggregateExpression.scala
@@ -1,3 +1,23 @@
+/*
+ * gcore-spark is the reference implementation of the G-CORE graph query
+ * language by the Linked Data Benchmark Council (LDBC) - ldbcouncil.org
+ *
+ * The copyrights of the source code in this file belong to:
+ * - CWI (www.cwi.nl), 2017-2018
+ *
+ * This software is released in open source under the Apache License, 
+ * Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package algebra.expressions
 
 abstract class AggregateExpression(distinct: Boolean, expr: AlgebraExpression, symbol: String)
