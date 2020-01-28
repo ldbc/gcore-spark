b5722406372472d4bc77cb748e7ca2bd415d1e30
unknown <daldana9@gmail.com>
2020-01-28 12:06:08 -0300

    Checking Jenkins with a single test

---
+++ src/test/scala/newtests/SimpleTest.scala
@@ -0,0 +1,19 @@
+package newtests
+
+import collection.mutable.Stack
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class SimpleTest extends FunSuite{
+  test("A simple test") {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    val oldSize = stack.size
+    val result = stack.pop()
+    assert(result === 2)
+    assert(stack.size === oldSize - 1)
+  }
+}
