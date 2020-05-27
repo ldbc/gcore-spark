ddbb16201cd02cb61427bc62ee006214ed837b6d
unknown <daldana9@gmail.com>
2020-05-26 22:08:03 -0400

    Added disclaimer

--- src/test/scala/queries/match/OptionalMatchTests.scala
+++ src/test/scala/queries/match/OptionalMatchTests.scala
@@ -18,7 +18,7 @@
  * limitations under the License.
  */
 
-package construct
+package `match`
 
 import algebra.AlgebraRewriter
 import algebra.expressions.Label
@@ -36,7 +36,7 @@ import spark.examples.SocialTestGraph
 import spark.sql.SqlRunner
 
 @RunWith(classOf[JUnitRunner])
-class MultipleTemplateTests extends FunSuite {
+class OptionalMatchTests extends FunSuite {
   val sparkSession: SparkSession = SparkSession
     .builder()
     .appName("G-CORE Runner")
@@ -50,48 +50,4 @@ class MultipleTemplateTests extends FunSuite {
   val parser: ParseStage = SpoofaxParser(ParseContext(context.catalog))
   val rewriter: RewriteStage = AlgebraRewriter(AlgebraContext(context.catalog))
   val target: RunTargetCodeStage = SqlRunner(context)
-
-  test("3.2.1 Multiple nodes construct"){
-    val query = "CONSTRUCT (n), (m) MATCH (n)-[e:HasInterest]->(m)"
-    val expectedPerson = Seq(
-      ("104"), ("103")
-    ).toDF("id")
-    val expectedTag = Seq(
-      ("106")
-    ).toDF("id")
-
-    val rewrited: AlgebraTreeNode = rewriter(parser(query))
-    val graph: PathPropertyGraph = target(rewrited)
-
-    val resultPerson: DataFrame = graph.tableMap(Label("Person")).asInstanceOf[Table[DataFrame]].data
-    assert(resultPerson.select("id").except(expectedPerson).count == 0)
-
-    val resultTag: DataFrame = graph.tableMap(Label("Tag")).asInstanceOf[Table[DataFrame]].data
-    assert(resultTag.select("id").except(expectedTag).count == 0)
-  }
-
-  test("3.2.2 Node and edge construct"){
-    val query = "CONSTRUCT (n)-[e]->(m), (q) MATCH (n)-[e:HasInterest]->(m), (q:Place)"
-    val expectedPerson = Seq(
-      ("104"), ("103")
-    ).toDF("id")
-    val expectedTag = Seq(
-      ("106")
-    ).toDF("id")
-    val expectedPlace = Seq(
-      ("105"),("115")
-    ).toDF("id")
-
-    val rewrited: AlgebraTreeNode = rewriter(parser(query))
-    val graph: PathPropertyGraph = target(rewrited)
-
-    val resultPerson: DataFrame = graph.tableMap(Label("Person")).asInstanceOf[Table[DataFrame]].data
-    assert(resultPerson.select("id").except(expectedPerson).count == 0)
-
-    val resultTag: DataFrame = graph.tableMap(Label("Tag")).asInstanceOf[Table[DataFrame]].data
-    assert(resultTag.select("id").except(expectedTag).count == 0)
-
-    val resultPlace: DataFrame = graph.tableMap(Label("Place")).asInstanceOf[Table[DataFrame]].data
-    assert(resultPlace.select("id").except(expectedPlace).count == 0)
-  }
 }
